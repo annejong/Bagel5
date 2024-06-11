1c98647b80c34e5573eb17c6d4701c5eece3831c
annejong <annedejong123@gmail.com>
2024-06-11 14:00:45 +0200

    Commit June 06 2024

---
+++ db_proteins/00.formatall.sh
@@ -0,0 +1,10 @@
+formatdb -p T -o T -i bagel4_class1bacteriocin_db.faa
+formatdb -p T -o T -i bagel4_class2bacteriocin_db.faa
+formatdb -p T -o T -i bagel4_class3bacteriocin_db.faa
+formatdb -p T -o T -i all_context_db.faa
+formatdb -p T -o T -i bagel5_class12_bacteriocins_db.faa
+formatdb -p T -o T -i bagel5_class3_bacteriocins_db.faa
+formatdb -p T -o T -i bagel5_class12_putative_bacteriocins_db.faa
+# Ripp precursor Recognition Elements RREs
+formatdb -p T -o T -i bagel5_RRE_db.faa
+
